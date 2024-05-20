f7f71d33c28738e30dcaee92fea8a85c3a6707db
sjesus <s.jesus10@yahoo.com>
2024-05-20 15:42:53 -0500

    data binding with interpolation in catalog component

---
+++ src/app/catalog/catalog.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Iproduct } from './product.model';
+
+@Component({
+  selector: 'bot-catalog',
+  templateUrl: './catalog.component.html',
+  styleUrls: ['./catalog.component.css']
+})
+export class CatalogComponent {
+  product: Iproduct;
+
+  constructor() {
+    this.product = {
+      id: 2,
+      description:
+        "a friendly robot head with two eyes and a smile -- great for domestic use.",
+      name: "Friendly Bot",
+      imageName: "head-friendly.png",
+      category: "Heads",
+      price: 945.0,
+      discount: 0.2,
+    }
+  }
+}
---
+++ src/app/catalog/product.model.ts
@@ -0,0 +1,9 @@
+export interface Iproduct {
+    id: number;
+    description: string;
+    name: string;
+    imageName: string;
+    category: string;
+    price: number;
+    discount: number;
+}
